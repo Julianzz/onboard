b5336b951f85c89e3f833f278005a7711a577cd1
liuzhenzhong <liuzhenzhong@p1.com>
2017-12-27 11:24:06 +0800

    add run info into readme

--- README.md
+++ README.md
@@ -6,16 +6,49 @@ Adding users and swiping other people in order to find a match.
 ## Setup Env
 
 * install brew on mac, please following the link https://brew.sh/
-* brew upgrade && brew update && brew install golang 
-* brew install postgres
+        
+        brew upgrade && brew update && brew install golang 
+
+
 * set GOPATH variable to go local path, liking "export GOPATH=~/gopath"
- 
+
+        export GOPATH=~/gopath
+        mkdir -p $GOPATH
+
+* install and start postgres
+    
+        brew install postgres
+        export DATAPATH=/tmp/postdata
+        mkdir  -p $DATAPATH
+        initdb -D $DATAPATH
+        pg_ctl -D $DATAPATH start
+
+
 
 ## Build & Run
 
-* clone code into golang path path: $GOPATH/src/github.com/p1cn/onboard/liuzhenzhong
-* start postgres
+* clone code into golang path path: 
+
+        export ONBOARD_PATH="$GOPATH/src/github.com/p1cn/onboard"
+        mkdir -p $ONBOARD_PATH
+        cd $ONBOARD_PATH
+        git clone https://github.com/Julianzz/onboard liuzhenzhong
+        
+
 * change  postgres database config: conf/config.yml
-* create table: go run cmd/database/initdb.go
-* start server: go run cmd/server/onboard.go -port=":8090" 
+
+        db_settings:
+            host: "127.0.0.1:5432"
+            database: "test"
+            user: "liuzhenzhong"
+            password: ""
+
+* create table: 
+    
+        go run cmd/database/initdb.go
+
+* start server:
+
+        go run cmd/server/onboard.go -port=":8090" 
+
 
--- scripts/test.py
+++ scripts/test.py
@@ -1,33 +1,37 @@
 import requests
 import json
 
-def test_state(state1, state2):
+def put_relation(u1, u2, state):
+    url = "http://127.0.0.1:8080/users/"+u1+"/relationships/"+u2
+    data = {"state": state}
+    res = requests.put(url, json=data)
+    print(res.text, res)
+
+def get_users():
+    res = requests.get("http://127.0.0.1:8080/users")
+    print(res.text, res)
+
+def test_state(state1, state2, num=2):
     pre_user_id = ""
-    for i in range(2):
+    for i in range(num):
         data = {
             "name": "liuzhenzhong"
         }
         res = requests.post("http://127.0.0.1:8080/users", json=data)
-        print(res.text, res)
+        #print(res.text, res)
 
         value = json.loads(res.text)
         if pre_user_id:
-            url = "http://127.0.0.1:8080/users/"+value["id"]+"/relationships/"+pre_user_id
-            data = {"state": state1}
-            res = requests.put(url, json=data)
-            print(res.text, res)
-
-            url = "http://127.0.0.1:8080/users/"+pre_user_id+"/relationships/"+value["id"]
-            data = {"state": state2}
-            res = requests.put(url, json=data)
-            print(res.text, res)
-
-        print("------get relations:")
-        url = "http://127.0.0.1:8080/users/"+value["id"]+"/relationships"
-        res = requests.get(url)
-        print(res.text, res)
+            put_relation(value["id"], pre_user_id, state1)
+            put_relation(pre_user_id, value["id"], state2)
 
         if pre_user_id:
+
+            print("------get relations:")
+            url = "http://127.0.0.1:8080/users/"+value["id"]+"/relationships"
+            res = requests.get(url)
+            print(res.text, res)
+
             url = "http://127.0.0.1:8080/users/"+pre_user_id+"/relationships"
             res = requests.get(url)
             print(res.text, res)     
@@ -38,5 +42,8 @@ print("")
 print("")
 test_state("liked", "liked")
 
-res = requests.get("http://127.0.0.1:8080/users")
-print(res.text, res)
+print("----------putwront userid")
+put_relation("23", "25", "liked")
+
+print("----------putwront state")
+test_state("matched", "liked")
